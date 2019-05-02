2bb6c4f44eca24b067e91fbb8fbf74a3fa2f67e2
Artem <48565841+arstrel@users.noreply.github.com>
2019-05-02 02:01:02 -0400

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysqli_connect("shareddb-o.hosting.stackcp.net",
-"Arstrel", "MirMvo5Data", "myusers-3130398406");
-
-if (mysqli_connect_error()) {
-  die ("Database Connection Error");
- }
-?>
